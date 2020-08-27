115bba330fad12a303eb516286f0353cfc988b42
fizza <fizza148168@gmail.com>
2020-08-27 14:32:20 +0500

    task done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const hello= ({name})=>
 function App({name,lastName,age}) {
   return <div className="myapp">
          Hello from  <strong>{name}</strong>
-         <span>&#128512; &#128516; &#128525; &#128151;</span> <br/>
+         <span role="img" aria-label="Emojis">&#128512; &#128516; &#128525; &#128151;</span> <br/>
          <ul>
             <li>{name}</li>
             <li>{lastName}</li>
