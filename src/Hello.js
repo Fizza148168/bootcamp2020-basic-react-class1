f18f54467946db7aa1e7e59dd61aca058a165afb
fizza <fizza148168@gmail.com>
2020-08-27 13:34:04 +0500

    basic react app

---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+function Hello({fName}) {
+return <p className="myname" >This is Para in Hello Tag name: {fName}</p>
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './App'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  <div><App name="Fizza" lastName="Shabbir" age={23}/></div>,
+    document.getElementById('root')
+  );
+ /*
+ ReactDOM.render(
+  React.createElement('div',null,React.createElement('span',null,"Hello in span create element")),
+  This is equivalent to jsx given below
+  <div>
+  <span>
+  Hello in span create element
+  </span>
+  </div>
+    document.getElementById('root')
+  );
+   */
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
